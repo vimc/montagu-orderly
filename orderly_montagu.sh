273c1501dbc3467c52279d509f21bc13e70914ff
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-08-17 17:22:45 +0100

    Initial import based on montagu-reports:908c0b0
    See VIMC-517

---
+++ orderly_montagu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker run --rm \
+       -v montagu_orderly_volume:/orderly \
+       -w /orderly \
+       --network montagu_default \
+       docker.montagu.dide.ic.ac.uk:5000/montagu-reports-runner:master $*
---
+++ orderly_montagu_sh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ex
+if [ "$#" -eq 0 ]; then
+    CMD=bash
+else
+    CMD=$1
+    shift
+fi
+
+docker run --rm \
+       -it --entrypoint $CMD \
+       -v montagu_orderly_volume:/orderly \
+       -w /orderly \
+       --network montagu_default \
+       docker.montagu.dide.ic.ac.uk:5000/montagu-reports-runner:master $*
