6fbdd7e7107c931baad30b753c83b6627f11e970
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-09-20 16:31:35 +0100

    Update scripts to reflect constantly running container

--- orderly_montagu.sh
+++ orderly_montagu.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-docker run --rm \
-       -v montagu_orderly_volume:/orderly \
-       -w /orderly \
-       --network montagu_default \
-       docker.montagu.dide.ic.ac.uk:5000/montagu-orderly:master $*
+docker exec -it montagu_orderly_1 orderly $*
